1c7c81f2ea51b631bde6e057f10b8d643d36fa33
anirbanmajumder0 <81350357+anirbanmajumder0@users.noreply.github.com>
2023-03-20 23:22:15 +0530

    changed links

--- frontend/components/Widgets.tsx
+++ frontend/components/Widgets.tsx
@@ -13,19 +13,19 @@ function Widgets() {
         <FireIcon className='relative h-8 w-8 top-3'/>
         <p className='inline-flex group-hover:text-twitter text-xl font-normal'>Here Are Some <br></br>Awesome Links:</p>
         </div>
-        <div onClick={() => window.open('https://www.anirbanmajumder.tech/')} className='col-span-2 flex max-w-fit space-x-2 px-4 py-4 rounded-full hover:bg-bordercolor cursor-pointer transition duration-200 group'>
+        <div onClick={() => window.open('https://anirbanmajumder.vercel.app/')} className='col-span-2 flex max-w-fit space-x-2 px-4 py-4 rounded-full hover:bg-bordercolor cursor-pointer transition duration-200 group'>
         <SparklesIcon className='h-6 w-6'/>
         <p className='inline-flex group-hover:text-twitter text-xl font-light'>My Website</p>
         </div>
-        <div onClick={() => window.open('https://www.anirbanmajumder.tech/about')} className='col-span-2 flex max-w-fit space-x-2 px-4 py-4 rounded-full hover:bg-bordercolor cursor-pointer transition duration-200 group'>
+        <div onClick={() => window.open('https://anirbanmajumder.vercel.app/about')} className='col-span-2 flex max-w-fit space-x-2 px-4 py-4 rounded-full hover:bg-bordercolor cursor-pointer transition duration-200 group'>
         <InformationCircleIcon className='h-6 w-6'/>
         <p className='inline-flex group-hover:text-twitter text-xl font-light'>About Me</p>
         </div>
-        <div onClick={() => window.open('https://blog.anirbanmajumder.tech/')} className='col-span-2 flex max-w-fit space-x-2 px-4 py-4 rounded-full hover:bg-bordercolor cursor-pointer transition duration-200 group'>
+        <div onClick={() => window.open('https://blog-anirbanmajumder0.vercel.app/')} className='col-span-2 flex max-w-fit space-x-2 px-4 py-4 rounded-full hover:bg-bordercolor cursor-pointer transition duration-200 group'>
         <NewspaperIcon className='h-6 w-6'/>
         <p className='inline-flex group-hover:text-twitter text-xl font-light'>My Blog</p>
         </div>
-        <div onClick={() => window.open('mailto: contact@anirbanmajumder.tech')} className='col-span-2 flex max-w-fit space-x-2 px-4 py-4 rounded-full hover:bg-bordercolor cursor-pointer transition duration-200 group'>
+        <div onClick={() => window.open('mailto: anirbanchess2015@gmail.com')} className='col-span-2 flex max-w-fit space-x-2 px-4 py-4 rounded-full hover:bg-bordercolor cursor-pointer transition duration-200 group'>
         <MailIcon className='h-6 w-6'/>
         <p className='inline-flex group-hover:text-twitter text-xl font-light'>Contact Me</p>
         </div>
