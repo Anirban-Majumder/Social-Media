2f8e283fb0e0bbfd757d7a070a123f8fdeada030
anirbanmajumder0 <81350357+anirbanmajumder0@users.noreply.github.com>
2023-03-26 13:33:14 +0530

    terms and privacy update

--- frontend/pages/api/auth/[...nextauth].js
+++ frontend/pages/api/auth/[...nextauth].js
@@ -2,22 +2,54 @@ import NextAuth from 'next-auth';
 import GoogleProvider from "next-auth/providers/google";
 import FacebookProvider from "next-auth/providers/facebook";
 import GithubProvider from "next-auth/providers/github";
+import CredentialsProvider from "next-auth/providers/credentials";
 import { SanityAdapter} from 'next-auth-sanity';
 import { sanityClient } from '../../../sanity';
 
 const options = {
   providers: [
+    CredentialsProvider({
+      name: 'email',
+      credentials: {
+        email: { label: "Email", type: "email", placeholder: "Email" },
+        password: { label: "Password", type: "password", placeholder: "Password" }
+      },async authorize(credentials) {
+        const { email, password } = credentials;
+        // before these add the password hash field optional field to the sanity database
+        // add code to fetch password hash from sanity database of that user/email
+        // compare password hash with password
+        // if password is correct then return user object  //create api to fetch user object from sanity database
+        //const user = { id: 1, name: 'J Smith', email: 'jsmith@example.com' }
+        const res = await fetch("/your/endpoint", {
+          method: 'POST',
+          body: JSON.stringify(credentials),
+          headers: { "Content-Type": "application/json" }
+        })
+        const user = await res.json()
+
+        if (res.ok && user) {
+          // Any object returned will be saved in `user` property of the JWT
+          return user
+        } else {
+          // If you return null or false then the credentials will be rejected
+          return null
+          // You can also Reject this callback with an Error or with a URL:
+          // throw new Error('error message') // Redirect to error page
+          // throw '/path/to/redirect'        // Redirect to a URL
+        }
+      }
+    }),
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
-    FacebookProvider({
-      clientId: process.env.FACEBOOK_CLIENT_ID,
-      clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
-    }),
     GithubProvider({
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    }),
+    FacebookProvider({
+      clientId: process.env.FACEBOOK_CLIENT_ID,
+      clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
     }),
   ],
   pages: {
--- frontend/pages/signin.tsx
+++ frontend/pages/signin.tsx
@@ -1,10 +1,47 @@
 import { signIn, useSession } from 'next-auth/react';
 import { getProviders } from 'next-auth/react';
 import React, { useEffect, useState } from 'react';
+import toast from 'react-hot-toast';
+
+// add a create user api and new user form
+// add a reset password api and form
 
 export default function SignIn(): JSX.Element {
   const [providers, setProviders] = useState<any>(null);
   const { data: session } = useSession();
+  const [emailLogin, setEmailLogin] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async (event: any) => {
+    event.preventDefault();
+    if (email === '') {
+      toast.error('Email is required');
+      return;
+    } else if (password === '') {
+      toast.error('Password is required');
+      return;
+    } else if (!email.includes('@')) {
+      toast.error('Email is not valid');
+      return;
+    } else if (password.length < 8) {
+      toast.error('Password is not valid');
+      return;
+    } else if (password.length > 20) {
+      toast.error('Password is not valid');
+      return;
+    //} else if (emailExist(email)) {
+    //  toast.error('Email does not exist'); // write a util+ api  to check if email exist
+    //  return;
+    //} else if (emailFromOtherProvider(email)) {
+    //  toast.error('Please login with your provider'); // write a uti+api to check if email from other provider
+    //  return;
+    } else {
+      signIn('email', { email, password });
+      toast.success('Login Successful');
+      return;
+    }
+  };
 
   function getProviderLogo(providerName: string) {
     switch (providerName) {
@@ -14,9 +51,17 @@ export default function SignIn(): JSX.Element {
         return 'https://img.icons8.com/fluency/48/null/facebook-new.png';
       case 'GitHub':
         return 'https://img.icons8.com/ios-filled/50/null/github.png';
+      case 'email':
+        return 'https://img.icons8.com/fluency/48/null/filled-message.png';
       default:
         return '/default-logo.png';
     }
+  }
+
+  function errorToast() {
+    //toast.dismiss();
+    //toast.success('Email login is not available yet');
+    alert('Email login is not available yet');
   }
 
   useEffect(() => {
@@ -32,32 +77,69 @@ export default function SignIn(): JSX.Element {
   }
 
   return (
-    <div className="flex flex-col justify-between min-h-screen py-2">
-      <div className="flex flex-col items-center justify-center pt-8">
+    <div className="flex flex-col justify-between min-h-screen py-4 items-center dark:bg-darkgrey">
+      <div className="flex flex-col items-center justify-center mt-16">
         <h2 className="text-3xl font-bold mb-4">Welcome back.</h2>
       </div>
+      {!emailLogin?
       <div className="flex flex-col items-center justify-center">
-      {Object.values(providers).map((provider) => (
-            <div key={provider.name} className="mb-4">
+      {Object.values(providers).map((provider:any) => (
+            <div key={provider.name} className="mb-4 flex items-center justify-start">
               <button
-                onClick={() => signIn(provider.id,{ callbackUrl: '/' })}
-                className="w-60 px-4 py-2 rounded-full border border-gray-600 dark:border-gray-300 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 whitespace-nowrap flex items-center justify-center"
+                onClick={!(provider.name==="email")?() => signIn(provider.id,{ callbackUrl: '/' }) : errorToast}
+                className="w-60 px-4 py-2 rounded-full border border-gray-600 dark:border-gray-400 hover:border-black dark:hover:border-white transition ease-in-out hover:-translate-y-1 hover:scale-80 duration-300 whitespace-nowrap flex items-center justify-center"
               >
-                <img src={getProviderLogo(provider.name)} alt={provider.name} className="w-6 h-6 mr-2" />
+                <img src={getProviderLogo(provider.name)} alt={provider.name} className="w-6 h-6 mr-2 left-0" />
                 Sign in with {provider.name}
               </button>
             </div>
           ))}
-      </div>
+      </div>:
+      <form onSubmit={handleSubmit} className="items-center shadow-md rounded-xl px-8 pt-6 pb-8 mb-4 w-80 dark:shadow-gray-400">
+        <div className="mb-4">
+          <label className="block text-sm font-bold mb-2" htmlFor="email">
+            Email
+          </label>
+          <input
+            className="shadow appearance-none border rounded-full w-full py-2 px-3 bg-boxcolor dark:bg-darkboxcolor leading-tight focus:outline-none focus:shadow-outline"
+            id="email"
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+          />
+        </div>
+        <div className="mb-6">
+          <label className="block text-sm font-bold mb-2" htmlFor="password">
+            Password
+          </label>
+          <input
+            className="shadow appearance-none border rounded-full w-full py-2 px-3 bg-boxcolor dark:bg-darkboxcolor mb-3 leading-tight focus:outline-none focus:shadow-outline"
+            id="password"
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
+        </div>
+        <div className="flex flex-grow items-center justify-between text-center">
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="submit"
+          >
+            Sign In
+          </button>
+        </div>
+      </form>}
       <div className="mb-4 pb-4">
         <p className="text-sm text-gray-600 text-center">
           No account?{' '}
-          <button className="font-bold text-twitter hover:text-blue-400 focus:outline-none focus:ring-offset-transparent focus:ring-blue-200 focus:ring-opacity-50">
+          <button className="font-bold text-twitter transition ease-in-out hover:-translate-y-1 hover:scale-80 duration-300">
             Create one
           </button>
         </p>
       </div>
-      <div className="mb-4 text-center">
+      <div className="mb-4 text-center px-4">
         <p className="text-xs text-textgray">
           Click “Sign In” to agree to Ani’s place’s{' '}
           <a
@@ -78,6 +160,17 @@ export default function SignIn(): JSX.Element {
             Privacy Policy
           </a>{' '}
           applies to you.
+        </p>
+        <p className="text-xs text-textgray">
+          Icons by{' '}
+          <a
+            href="https://icons8.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:text-twitter underline"
+          >
+            Icons8
+          </a>
         </p>
       </div>
     </div>
